0eaaabcc5628b4ee470724c740d6795f9e5e772a
jimmy_shen <jim.morris.shen@gmail.com>
2019-09-11 19:50:51 -0400

    add one program to solve the sorting in abc order homework

---
+++ week1_09092019/words.py
@@ -0,0 +1,9 @@
+"""
+Input 10 words and put them in to alphabet order
+Katherine Shen
+Sep 11, 2019
+"""
+word_list = ['card', 'park', 'hard', 'barn', 'yard', 'shark', 'smart', 'charge', 'large', 'sharp']
+print('Before sorting in abc order', word_list)
+word_list.sort()
+print('After sorting in abc order', word_list)
