a9d61ac1c8ad84e500b03710eb18ee2857cfe1da
jimmy_shen <jim.morris.shen@gmail.com>
2019-09-13 18:42:00 -0400

    update the comments

--- week1_09092019/words.py
+++ week1_09092019/words.py
@@ -1,6 +1,7 @@
 """
 Input 10 words and put them in to alphabet order
 Katherine Shen
+First grade student of PS 8
 Sep 11, 2019
 """
 word_list = ['card', 'park', 'hard', 'barn', 'yard', 'shark', 'smart', 'charge', 'large', 'sharp']
