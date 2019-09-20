6213d040a561652da0441ce9266bc983a77febf5
jimmy_shen <jim.morris.shen@gmail.com>
2019-09-20 11:15:49 -0400

    update

---
+++ week_of_09192019/words.py
@@ -0,0 +1,10 @@
+"""
+Input 10 words and put them in to alphabet order
+Katherine Shen
+First grade student of PS 8
+Sep 19, 2019
+"""
+word_list = []
+print('Before sorting in abc order', word_list)
+word_list.sort()
+print('After sorting in abc order', word_list)
